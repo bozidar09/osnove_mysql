27a472cd33a897bddd18d735a582d72f80ef8601
bozidar <bozidar.markovic09@mail.com>
2024-06-25 14:47:48 +0200

    videoteka update

--- 05_videoteka/videoteka_procedure.sql
+++ 05_videoteka/videoteka_procedure.sql
@@ -1,42 +1,59 @@
-DELIMITER //
 
-CREATE PROCEDURE create_posudba(IN p_clan_id INT, IN p_film_id INT, IN p_medij_id INT)
+DELIMITER $$
+
+CREATE PROCEDURE create_posudba(
+    IN p_clan_id INT UNSIGNED,
+    IN p_kopija_id INT UNSIGNED
+)
+
 BEGIN
-    DECLARE v_kolicina INT;
-    DECLARE v_zaliha_id INT;
 
-    -- Ensure the film is in stock
-    SELECT kolicina INTO v_kolicina 
-    FROM zaliha 
-    WHERE film_id = p_film_id AND medij_id = p_medij_id 
+    DECLARE v_posudba_id INT UNSIGNED;
+    DECLARE v_kolicina INT;
+
+    -- Start transaction
+    START TRANSACTION;
+
+    -- Ensure the film copy is available
+    SELECT count(f.id)
+        INTO v_kolicina
+        FROM kopija k
+        JOIN filmovi f ON k.film_id = f.id
+        JOIN mediji m ON k.medij_id = m.id
+        WHERE m.tip = 'DVD' 
+            AND k.dostupan = 1
+            AND f.naslov = 'Inception'
+        GROUP BY f.id
     FOR UPDATE;
 
-    -- Check if there is stock available
     IF v_kolicina > 0 THEN
-        -- Retrieve the zaliha_id
-        SELECT id INTO v_zaliha_id 
-        FROM zaliha 
-        WHERE film_id = p_film_id AND medij_id = p_medij_id 
-        LIMIT 1;
-
         -- Insert the new borrowing record
-        INSERT INTO posudba (datum_posudbe, datum_povrata, clan_id, zaliha_id) 
-        VALUES (CURDATE(), DATE_ADD(CURDATE(), INTERVAL 1 DAY), p_clan_id, v_zaliha_id);
+        INSERT INTO posudba (datum_posudbe, clan_id)
+        VALUES (CURDATE(), p_clan_id);
 
-        -- Update the stock quantity
-        UPDATE zaliha 
-        SET kolicina = kolicina - 1 
-        WHERE id = v_zaliha_id;
+        SET v_posudba_id = LAST_INSERT_ID();
 
+        -- Insert the kopija into posudba_kopija
+        INSERT INTO posudba_kopija (posudba_id, kopija_id)
+        VALUES (v_posudba_id, p_kopija_id);
+
+        -- Update the availability of the kopija
+        UPDATE kopija
+        SET dostupan = 0
+        WHERE id = p_kopija_id;
+
+        -- Commit the transaction
         COMMIT;
     ELSE
-        -- If there is no stock available, rollback the transaction
+        -- If a film copy is not available, rollback the transaction
         ROLLBACK;
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Not enough stock available';
     END IF;
-END //
+    
+END $$
 
 DELIMITER ;
 
--- (clan_id, film_id, medij_id)
-CALL create_posudba(1, 6, 2);
+
+-- (clan_id, kopija_id)
+CALL create_posudba(1, 2);
