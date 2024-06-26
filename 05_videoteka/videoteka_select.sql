a98de14238813f37d9f86e6be72ceed7eb77b3b4
bozidar <bozidar.markovic09@mail.com>
2024-06-26 15:56:49 +0200

    videoteka select ispravak

--- 05_videoteka/videoteka_select.sql
+++ 05_videoteka/videoteka_select.sql
@@ -8,18 +8,18 @@ SELECT * FROM clanovi;
 SELECT naslov AS Naslov, godina as 'Godina izdanja' FROM filmovi;
 
 -- dohvati samo prvi zapis iz tablice filmovi
-SELECT * FROM filmovi WHERE 1;
+SELECT * FROM filmovi LIMIT 1;
 
 -- dohvati zapis iz tablice filmovi sa točno određenim naslovom
-SELECT * FROM filmovi WHERE naslov='inception';
+SELECT * FROM filmovi WHERE naslov='Inception';
 
 -- dohvati zapis iz tablice filmovi gdje je id 2 ili 3 
 SELECT * FROM filmovi WHERE id=2 OR id=3;
 SELECT * FROM filmovi WHERE id IN (2,3);
 SELECT * FROM filmovi WHERE id BETWEEN 2 AND 3;
 
--- dohvati zapis iz tablice clanovi gdje je ime Ivan i telefon je '5585829592'
-SELECT * FROM clanovi WHERE ime='Ivan Horvant' AND telefon='5585829592';
+-- dohvati zapis iz tablice clanovi gdje je ime Ivan i telefon je '0912345678'
+SELECT * FROM clanovi WHERE ime='Ivan Horvat' AND telefon='0912345678';
 
 -- prioritet operatora, AND ima veći prioritet
 SELECT * FROM filmovi WHERE (id=1 OR id=2) AND naslov='Kum';
@@ -38,16 +38,16 @@ SELECT * FROM filmovi ORDER BY godina DESC;
 -- pretraži tablicu filmovi po dijelu naslova filma ('_' -> zamjenjuje točno jedan proizvoljan znak, a '%' -> 0, jedan ili više proizvoljnih)
 SELECT * FROM filmovi WHERE naslov LIKE '_nce%';
 
--- count, avg, sum
-SELECT count(id) AS "Broj filmova u bazi" FROM filmovi;
-SELECT count(id) AS "Broj filmova u bazi mladjih od 1990" FROM filmovi WHERE godina > 1990;
+-- COUNT, AVG, SUM
+SELECT COUNT(id) AS "Broj filmova u bazi" FROM filmovi;
+SELECT COUNT(id) AS "Broj filmova u bazi mladjih od 1990" FROM filmovi WHERE godina > 1990;
 
-SELECT avg(cijena) AS "Prosjek cijene" FROM cjenik;
-SELECT format(avg(cijena), 2) AS "Prosjek cijene formatiran" FROM cjenik; -- druga opcija round() umjesto format, vraća int umjesto char
+SELECT AVG(cijena) AS "Prosjek cijene" FROM cjenik;
+SELECT FORMAT(AVG(cijena), 2) AS "Prosjek cijene formatiran" FROM cjenik; -- druga opcija ROUND() umjesto format, vraća INT umjesto CHAR
 
-SELECT sum(cijena) AS "Ukupna cijena" FROM cjenik; 
+SELECT SUM(cijena) AS "Ukupna cijena" FROM cjenik; 
 
--- inner join (jedan i više njih u jednoj select naredbi)
+-- INNER JOIN (jedan i više njih u jednoj select naredbi)
 SELECT f.naslov, f.godina, z.ime AS 'zanr'
     FROM filmovi f -- alias može i bez AS
     JOIN zanrovi z ON f.zanr_id = z.id
@@ -57,50 +57,60 @@ SELECT f.naslov, f.godina, z.ime AS 'zanr', c.cijena AS 'cijena', c.zakasnina_po
     JOIN zanrovi z ON f.zanr_id = z.id
     JOIN cjenik c ON f.cjenik_id = c.id;
 
--- join sa funkcijom DATEDIFF (uz nju može LTRIM, CONCAT, a može i bez)
-SELECT cl.ime AS 'Ime', f.naslov AS 'Film' , p.datum_posudbe AS 'Datum posudbe', p.datum_povrata AS 'Datum povrata', CONCAT(DATEDIFF(p.datum_povrata, p.datum_posudbe)-1) AS 'Kašnjenje (dana)', CONCAT((DATEDIFF(p.datum_povrata, p.datum_posudbe)-1) * cj.zakasnina_po_danu + cj.cijena) AS 'Cijena'
-    FROM posudba as p 
-    JOIN clanovi as cl ON p.clan_id = cl.id 
-    JOIN zaliha as z ON p.zaliha_id = z.id
-    JOIN filmovi AS f ON z.film_id = f.id
-    JOIN cjenik AS cj ON f.cjenik_id = cj.id
-    WHERE DATEDIFF(p.datum_povrata, p.datum_posudbe) >= 2; 
+-- JOIN sa funkcijom DATEDIFF (uz nju može LTRIM, CONCAT, a može i bez)
+SELECT 
+    cl.ime AS 'Ime', 
+    f.naslov AS 'Film', 
+    ps.datum_posudbe AS 'Datum posudbe', 
+    ps.datum_povrata AS 'Datum povrata', 
+    CONCAT(DATEDIFF(ps.datum_povrata, ps.datum_posudbe)-1) AS 'Kašnjenje (dana)', 
+    CONCAT((DATEDIFF(ps.datum_povrata, ps.datum_posudbe)-1) * cj.zakasnina_po_danu + cj.cijena) AS 'Cijena'
+        FROM posudba_kopija AS pk 
+        JOIN posudba AS ps ON pk.posudba_id = ps.id
+        JOIN clanovi AS cl ON ps.clan_id = cl.id 
+        JOIN kopija AS k ON pk.kopija_id = k.id 
+        JOIN filmovi AS f ON k.film_id = f.id
+        JOIN cjenik AS cj ON f.cjenik_id = cj.id
+        WHERE DATEDIFF(ps.datum_povrata, ps.datum_posudbe) >= 2; 
 
--- spoji tablice posudba sa filmovi i mediji preko vezne tablice zaliha
-SELECT p.datum_posudbe, p.datum_povrata, c.ime, f.naslov, m.tip
-    FROM posudba p
-    JOIN clanovi c ON p.clan_id = c.id
-    JOIN zaliha z ON p.zaliha_id = z.id
-    JOIN filmovi f ON z.film_id = f.id
-    JOIN mediji m ON z.medij_id = m.id;
+-- spoji tablicu posudba sa kopija, filmovi i mediji preko vezne tablice posudba_kopija
+SELECT ps.datum_posudbe, ps.datum_povrata, cl.ime, f.naslov, m.tip
+    FROM posudba_kopija AS pk 
+    JOIN posudba AS ps ON pk.posudba_id = ps.id
+    JOIN clanovi AS cl ON ps.clan_id = cl.id 
+    JOIN kopija AS k ON pk.kopija_id = k.id 
+    JOIN filmovi AS f ON k.film_id = f.id
+    JOIN mediji AS m ON k.medij_id = m.id;
 
 
 -- izlistaj posudbe i film naziv i medij za filmove koji nisu vraćeni
-SELECT p.datum_posudbe, p.datum_povrata, c.ime, f.naslov, m.tip
-    FROM posudba p
-    JOIN clanovi c ON p.clan_id = c.id
-    JOIN zaliha z ON p.zaliha_id = z.id
-    JOIN filmovi f ON z.film_id = f.id
-    JOIN mediji m ON z.medij_id = m.id
-    WHERE p.datum_povrata IS NULL; -- sa NULL se uspoređuje pomoću 'IS NULL' i 'IS NOT NULL'
+SELECT ps.datum_posudbe, ps.datum_povrata, cl.ime, f.naslov, m.tip
+    FROM posudba_kopija AS pk 
+    JOIN posudba AS ps ON pk.posudba_id = ps.id
+    JOIN clanovi AS cl ON ps.clan_id = cl.id 
+    JOIN kopija AS k ON pk.kopija_id = k.id 
+    JOIN filmovi AS f ON k.film_id = f.id
+    JOIN mediji AS m ON k.medij_id = m.id
+    WHERE ps.datum_povrata IS NULL; -- sa NULL se uspoređuje pomoću 'IS NULL' i 'IS NOT NULL'
 
 -- povezi sve tablice i izlistaj podatke
 SELECT 
-    p.datum_posudbe, 
-    p.datum_povrata, 
-    c.ime AS 'ime clana', 
+    ps.datum_posudbe, 
+    ps.datum_povrata, 
+    cl.ime AS 'ime clana', 
     f.naslov, 
     m.tip AS 'medij', 
-    zanrovi.ime AS 'zanr', 
-    cjenik.tip_filma, 
-    cjenik.zakasnina_po_danu
-        FROM posudba p
-        JOIN clanovi c ON p.clan_id = c.id
-        JOIN zaliha z ON p.zaliha_id = z.id
-        JOIN filmovi f ON z.film_id = f.id
-        JOIN mediji m ON z.medij_id = m.id
-        JOIN zanrovi ON f.zanr_id = zanrovi.id
-        JOIN cjenik ON f.cjenik_id = cjenik.id;
+    z.ime AS 'zanr', 
+    cj.tip_filma, 
+    cj.zakasnina_po_danu
+        FROM posudba_kopija AS pk 
+        JOIN posudba AS ps ON pk.posudba_id = ps.id
+        JOIN clanovi AS cl ON ps.clan_id = cl.id 
+        JOIN kopija AS k ON pk.kopija_id = k.id 
+        JOIN mediji AS m ON k.medij_id = m.id
+        JOIN filmovi AS f ON k.film_id = f.id
+        JOIN cjenik AS cj ON f.cjenik_id = cj.id
+        JOIN zanrovi AS z ON f.zanr_id = z.id;
 
 -- ispiši članove koji su posudili više od jednog filma sa GROUP BY
 SELECT c.id, c.ime AS 'clan', COUNT(*) AS "broj posudbi" -- može i sa COUNT(c.id) jer broji retke
@@ -110,56 +120,57 @@ SELECT c.id, c.ime AS 'clan', COUNT(*) AS "broj posudbi" -- može i sa COUNT(c.i
     HAVING COUNT(*) > 1;
 
 -- ispišite totalnu količinu kopija dostupnih po filmu (zbroj svih medija) sa GROUP BY
-SELECT f.id, f.naslov, SUM(z.kolicina) AS "kolicina" -- kod SUM() se mora navesti točno određeni stupac koji će se zbrajati
+SELECT f.id, f.naslov, COUNT(k.dostupan) AS "kolicina" -- može i SUM() kod kojeg se mora navesti točno određeni stupac koji će se zbrajati
     FROM filmovi f
-    JOIN zaliha z ON f.id = z.film_id
+    JOIN kopija k ON f.id = k.film_id
     GROUP BY f.id;
 
--- filtirajte gornji upit da izlista samo filmove koji imaju više od 10 kopija sa HAVING
-SELECT f.id, f.naslov, SUM(z.kolicina) AS kolicina
+-- filtirajte gornji upit da izlista samo filmove koji imaju više od 5 kopija sa HAVING
+SELECT f.id, f.naslov, COUNT(f.id) AS kolicina
     FROM filmovi f
-    JOIN zaliha z ON f.id = z.film_id
+    JOIN kopija k ON f.id = k.film_id
     GROUP BY f.id
-    HAVING kolicina > 10; -- možemo iskoristiti alias u HAVING, ali on ne smije biti deklariran pod navodnicima
+    HAVING kolicina > 5; -- možemo iskoristiti alias u HAVING, ali on ne smije biti deklariran pod navodnicima
 
 -- sastavljanje podupita - dohvati sve filmove koji imaju količinu na Blueray-u
 -- prvi upit kojim dohvaćamo Blueray sa količinom
-SELECT z.film_id
-    FROM zaliha z
-    JOIN mediji m ON z.medij_id = m.id
-    WHERE m.tip = 'Blu-Ray';
-SELECT f.naslov
-    FROM filmovi f
-    WHERE f.id IN 
+SELECT GROUP_CONCAT(k.film_id)  -- funkcija konkatenira sve dobivene rezultate (odvojene zarezom)
+    FROM kopija k
+    JOIN mediji m ON k.medij_id = m.id
+    WHERE m.tip = 'Blu-Ray' AND k.dostupan = TRUE;
 -- drugi upit gdje tražimo naslove filma
 SELECT f.naslov
     FROM filmovi f
-    WHERE f.id IN ()
+    WHERE f.id IN (1,2,3,3,4,4,5,6,6);
 -- sastavljen upit s podupitom koji daje željeni rezultat
-SELECT z.film_id
-    FROM zaliha z
-    JOIN mediji m ON z.medij_id = m.id
-    WHERE m.tip = 'Blu-Ray';
+SELECT filmovi.naslov 
+    FROM filmovi 
+    WHERE filmovi.id IN (
+        SELECT k.film_id 
+            FROM kopija k 
+            JOIN mediji m ON k.medij_id = m.id 
+            WHERE m.tip = "Blu-Ray" AND k.dostupan = TRUE
+        );
 
--- dohvati članove koi imaju ne vraćen film preko jednog dana (zakasnina)
-SELECT c.*, DATEDIFF(p.datum_povrata, p.datum_posudbe) - 1 AS "zakasnina"
+-- dohvati članove koji imaju ne vraćen film preko jednog dana (kasnjenje)
+SELECT c.*, DATEDIFF(p.datum_povrata, p.datum_posudbe) - 1 AS "kasnjenje"
     FROM posudba p
     JOIN clanovi c ON c.id = p.clan_id
     WHERE DATEDIFF(p.datum_povrata, p.datum_posudbe) > 1 OR (p.datum_povrata IS NULL AND DATEDIFF(CURDATE(), p.datum_posudbe) > 1);
 
-SELECT c.*, DATEDIFF(p.datum_povrata, p.datum_posudbe) - 1 AS "zakasnina"
+SELECT c.*, DATEDIFF(p.datum_povrata, p.datum_posudbe) - 1 AS "kasnjenje"
     FROM posudba p
     JOIN clanovi c ON c.id = p.clan_id
     WHERE DATEDIFF(p.datum_povrata, p.datum_posudbe) > 1 OR DATEDIFF(IFNULL(p.datum_povrata, CURDATE()), p.datum_posudbe) > 1; -- drugi način sa IFNULL() funkcijom
 
--- dohvati zapise iz filmova, preskoči prva 2 zapisa, dohvati sveukupno 3 zapisa
+-- dohvati zapise iz filmova, preskoči prva 3 zapisa, dohvati sveukupno 2 zapisa
 SELECT * FROM filmovi LIMIT 2 OFFSET 3;  -- može i sa LIMIT 3, 2 -> obrnuto je kod korištenja sa zarezom
 
 -- if klauzula
-SELECT *, IF (dostupna = TRUE, 'dostupna', 'nema je') AS dostupna
+SELECT *, IF (dostupan = TRUE, 'dostupna', 'nema je') AS dostupna
     FROM kopija;
 
--- dohvati količinu svih filmova na određenom mediju
+-- dohvati količinu svih dostupnih filmova na određenom mediju (dvd)
 SELECT f.naslov, COUNT(f.id) 
 	FROM kopija k
 	JOIN filmovi f ON k.film_id = f.id
@@ -167,15 +178,15 @@ SELECT f.naslov, COUNT(f.id)
     WHERE m.tip = 'DVD' AND k.dostupan = 1
     GROUP BY f.id;
 
--- dohvati količinu filmova po svakom mediju za film ID 1 (uz konkateniranje rezultata)
-SELECT CONCAT(m.tip, ' ', COUNT(f.id)) 
+-- dohvati količinu dostupnih filmova po svakom mediju za film ID 1 (uz konkateniranje rezultata)
+SELECT CONCAT(m.tip, ' ', COUNT(f.id)) AS 'kolicina'
 	FROM kopija k
 	JOIN filmovi f ON k.film_id = f.id
     JOIN mediji m ON k.medij_id = m.id
     WHERE f.id = 1 AND k.dostupan = 1
     GROUP BY m.id;
 
--- za svaki film u bazi dohvatiti količinu filmova po mediju
+-- za svaki film u bazi dohvatiti količinu dostupnih filmova po mediju
 SELECT f.naslov, m.tip, COUNT(f.id) AS 'kolicina'
 	FROM kopija k
 	JOIN filmovi f ON k.film_id = f.id
@@ -200,7 +211,7 @@ SELECT p.*, c.ime, f.naslov
     JOIN kopija k ON k.id = pk.kopija_id
     JOIN filmovi f ON f.id = k.film_id;
 
-SELECT p.*, c.ime, IFNULL(f.naslov, 'nema posudbe') -- primjer korištenja IFNULL fukcije (na loš način)
+SELECT p.*, c.ime, IFNULL(f.naslov, 'nema posudbe') -- primjer korištenja IFNULL fukcije (na "loš" način, kako bi ispravili narušen integritet baze)
     FROM posudba p
     JOIN clanovi c ON c.id = p.clan_id
     LEFT JOIN posudba_kopija pk ON p.id = pk.posudba_id
@@ -246,7 +257,7 @@ UPDATE clanovi
 
 -- vremenske funkcije
 SELECT
-    NOW() AS trenutno_vrijeme_datum,
+    NOW() AS trenutni_datum_vrijeme,
     CURTIME() AS trenutno_vrijeme,
     CURDATE() AS trenutni_datum,
     CURRENT_TIMESTAMP;
